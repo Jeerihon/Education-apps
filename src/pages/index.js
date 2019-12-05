65d0810ddda2f39a3fa70554ec0e3639d0168d70
Artem Korytov <akoritov@neuron-team.ru>
2019-12-05 15:40:29 +0300

    Bin2Dec app done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import styles from '../styleguide.module.css';
+import styles from './index.module.css';
 
 export default () => {
-  // const [binaryDigit, setBinaryDigit] = useState();
+  const [decimal, setDecimal] = useState(10);
+  const [animate, setAnimate] = useState(false);
+  const [isValid, setIsValid] = useState(true);
 
-  const handleKeyDown = () => {
-    // const { value } = event.target.value;
-    // console.log(event, value);
+  const handleKeyDown = (event) => {
+    const { target: { value }, keyCode } = event;
+    const validKeyCodes = [48, 49, 37, 39, 8];
+
+    if (!validKeyCodes.includes(keyCode)) {
+      event.preventDefault(event);
+      setAnimate(true);
+      setIsValid(false);
+
+      setTimeout(() => {
+        setAnimate(false);
+      }, 300);
+      return;
+    }
+
+    setIsValid(true);
+
+    if (value.length === 7) setDecimal(parseInt(value, 2));
   };
 
   return (
@@ -15,28 +32,36 @@ export default () => {
       <div className={styles.container}>
         <header className={styles.header}>
           <h1 className={styles.title}>Bin2Dec</h1>
+        </header>
+
+        <main className={styles.content}>
           <p className={styles.description}>
             Bin2Dec allows binary digits and then displays its decimal
             equivalent.
           </p>
-        </header>
 
-        <main className={styles.content}>
           <div className={styles.item}>
             <span className={styles.label}>Enter binary digit:</span>
 
             <input
-              className={styles.input}
+              className={animate ? styles.inputInvalid : styles.input}
               type="text"
               onKeyDown={handleKeyDown}
               maxLength="8"
             />
+
+            {!isValid && <span className={styles.notification}>Only digits 0 and 1 approved</span>}
           </div>
 
           <div className={styles.item}>
             <span className={styles.label}>Get result:</span>
 
-            <input className={styles.result} type="number" readOnly />
+            <input
+              className={styles.result}
+              type="number"
+              value={decimal}
+              readOnly
+            />
           </div>
         </main>
       </div>
