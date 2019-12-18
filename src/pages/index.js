52d8a3f59c6d8d70e40bcd2498017d0bba51aff7
Artem Korytov <akoritov@neuron-team.ru>
2019-12-18 15:54:25 +0300

    WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'gatsby';
 
-import styles from './index.module.css';
+export default () => (
+  <>
+    <header>
+      <h1>Hello, Stranger! My name is Artem and this is my education projects.</h1>
+    </header>
 
-export default () => {
-  const [result, setResult] = useState(10);
-  const [animate, setAnimate] = useState(false);
-  const [isValid, setIsValid] = useState(true);
-
-  const handleKeyDown = (event) => {
-    const { keyCode } = event;
-    const validKeyCodes = [48, 49, 37, 39, 8];
-
-    if (!validKeyCodes.includes(keyCode)) {
-      event.preventDefault(event);
-      setAnimate(true);
-      setIsValid(false);
-
-      setTimeout(() => {
-        setAnimate(false);
-      }, 300);
-      return;
-    }
-
-    setIsValid(true);
-  };
-
-  const handleKeyUp = ({ target: { value } }) => {
-    setResult(parseInt(+value, 2));
-  };
-
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <h1 className={styles.title}>Bin2Dec</h1>
-        </header>
-
-        <main className={styles.content}>
-          <p className={styles.description}>
-            Bin2Dec allows binary digits and then displays its result
-            equivalent.
-          </p>
-
-          <div className={styles.item}>
-            <span className={styles.label}>Enter binary digit:</span>
-
-            <input
-              className={animate ? styles.inputInvalid : styles.input}
-              type="text"
-              onKeyDown={handleKeyDown}
-              onKeyUp={handleKeyUp}
-              maxLength="8"
-            />
-
-            {!isValid && <span className={styles.notification}>Only digits 0 and 1 approved</span>}
-          </div>
-
-          <div className={styles.item}>
-            <span className={styles.label}>Get result:</span>
-
-            <span className={styles.result}>{result}</span>
-          </div>
-        </main>
-      </div>
-    </div>
-  );
-};
+    <main>
+      <Link to="/bin2dec">bin2dec</Link>
+    </main>
+  </>
+);
