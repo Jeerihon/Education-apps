a8e71d84c0a9e1858ee61b5cef6777294e4c23b9
Artem Korytov <akoritov@neuron-team.ru>
2019-12-23 17:07:46 +0300

    added: routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-export default () => (
-  <>
-    <header>
-      <h1>Hello, Stranger! My name is Artem and this is my education projects.</h1>
-    </header>
+import styles from './index.module.css';
 
-    <main>
-      <Link to="/bin2dec">bin2dec</Link>
-    </main>
-  </>
+const APPS = [{
+  name: 'bin2dec',
+  route: '/bin2dec',
+  decription: 'Binary-to-Decimal number converter',
+}, {
+  name: 'Border Radius Previewer',
+  route: '/border-radius-previewer',
+  decription: 'Preview how CSS3 border-radius values affect an element',
+}];
+
+export default () => (
+  <div className={styles.wrapper}>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>
+          Hello, Stranger!
+          <br />
+          My name is Artem and this is my education projects.
+        </h1>
+      </header>
+
+      <main>
+        <ul>
+          {APPS.map((app) => (
+            <li>
+              <Link to={app.route}>{app.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  </div>
 );
