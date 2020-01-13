38c8b1e2c5952faaf9ec65ae3fcb191199fe04d5
Artem Korytov <akoritov@neuron-team.ru>
2020-01-13 11:40:41 +0300

    Added: apps grid

--- src/components/ItemsGrid/index.js
+++ src/components/ItemsGrid/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'gatsby';
 
 import styles from './index.module.css';
@@ -13,19 +13,35 @@ const APPS = [{
   decription: 'Preview how CSS3 border-radius values affect an element',
 }];
 
-export default () => (
-  <main className={styles.container}>
-    <ul className={styles.grid}>
-      {APPS.map((app) => (
-        <li className={styles.item}>
-          <Link
-            to={app.route}
-            key={app.name}
-          >
-            {app.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </main>
-);
+export default () => {
+  const handleScroll = () => {
+    const scrollTop = document.body.scrollTop
+      ? document.body.scrollTop
+      : document.scrollingElement.scrollTop;
+    const newPos = `${scrollTop}px`;
+    document.scrollingElement.style.setProperty('--scrollPos', newPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+
+  return (
+    <main className={styles.container}>
+      <ul className={styles.grid}>
+        {APPS.map((app) => (
+          <li className={styles.item} key={app.name}>
+            <Link className={styles.link} to={app.route}>
+              <span className={styles.appName}>{app.name}</span>
+              <span className={styles.description}>{app.decription}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
