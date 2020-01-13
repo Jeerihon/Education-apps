cf5cc079b7a2abc7a365578f07d2b3dab2689e6e
Artem Korytov <akoritov@neuron-team.ru>
2020-01-13 17:34:50 +0300

    Added: handlers position

--- src/pages/borderRadiusPreviewer.js
+++ src/pages/borderRadiusPreviewer.js
@@ -1,94 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './borderRadiusPreviewer.module.css';
 
-export default () => (
-// const [topLeft, setTopLeft] = useState(30);
-// const [topRight, setTopRight] = useState(70);
-// const [rightTop, setRightTop] = useState(70);
-// const [rightBottom, setRightBottom] = useState(30);
-// const [bottomRight, setBottomRight] = useState(30);
-// const [bottomLeft, setBottomLeft] = useState(0);
-// const [leftTop, setLeftTop] = useState(100);
-// const [leftBottom, setLeftBottom] = useState(70);
-  <div className={styles.wrapper}>
-    <div className={styles.container}>
-      <header className={styles.header}>
-        <h1 className={styles.title}>Border radius previewer</h1>
+export default () => {
+  const [topLeft] = useState(10);
+  const [topRight] = useState(16);
+  const [bottomRight] = useState(31);
+  const [bottomLeft] = useState(42);
+  const [leftTop] = useState(44);
+  const [rightTop] = useState(10);
+  const [rightBottom] = useState(50);
+  const [leftBottom] = useState(26);
 
-        <p className={styles.description}>
-          The border-radius property can have multiple values changed.
-          <br />
-          Preview how the shape looks while changing these values.
-        </p>
-      </header>
+  const borderRadius = `${topLeft}% ${topRight}% ${bottomRight}% ${bottomLeft}%
+    / ${leftTop}% ${rightTop}% ${rightBottom}% ${leftBottom}%`;
 
-      <main className={styles.content}>
-        <div className={styles.previewer}>
-          <div
-            className={styles.shape}
-            style={{ borderRadius: '10% 10% 10% 10% / 10% 10% 10% 10%' }}
-          />
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <h1 className={styles.title}>Border radius previewer</h1>
 
-          <span
-            className={styles.handle}
-            data-handle="top-left"
-            style={{ left: '10%' }}
-          />
+          <p className={styles.description}>
+            The border-radius property can have multiple values changed.
+            <br />
+            Preview how the shape looks while changing these values.
+          </p>
+        </header>
 
-          <span
-            className={styles.handle}
-            data-handle="top-right"
-            style={{ right: '10%' }}
-          />
+        <main className={styles.content}>
+          <div className={styles.previewer}>
+            <div
+              className={styles.shape}
+              style={{ borderRadius }}
+            />
 
-          <span
-            className={styles.handle}
-            data-handle="right-top"
-            style={{ top: '10%' }}
-          />
+            <span
+              className={styles.handle}
+              data-handle="top-left"
+              style={{ left: `${topLeft}%` }}
+            />
 
-          <span
-            className={styles.handle}
-            data-handle="right-bottom"
-            style={{ bottom: '10%' }}
-          />
+            <span
+              className={styles.handle}
+              data-handle="top-right"
+              style={{ right: `${topRight}%` }}
+            />
 
-          <span
-            className={styles.handle}
-            data-handle="bottom-right"
-            style={{ right: '10%' }}
-          />
+            <span
+              className={styles.handle}
+              data-handle="right-top"
+              style={{ top: `${rightTop}%` }}
+            />
 
-          <span
-            className={styles.handle}
-            data-handle="bottom-left"
-            style={{ left: '10%' }}
-          />
+            <span
+              className={styles.handle}
+              data-handle="right-bottom"
+              style={{ bottom: `${rightBottom}%` }}
+            />
 
-          <span
-            className={styles.handle}
-            data-handle="left-bottom"
-            style={{ bottom: '10%' }}
-          />
+            <span
+              className={styles.handle}
+              data-handle="bottom-right"
+              style={{ right: `${bottomRight}%` }}
+            />
 
-          <span
-            className={styles.handle}
-            data-handle="left-top"
-            style={{ top: '10%' }}
-          />
-        </div>
+            <span
+              className={styles.handle}
+              data-handle="bottom-left"
+              style={{ left: `${bottomLeft}%` }}
+            />
 
-        <div className={styles.copyBar}>
-          <span className={styles.copyLabel}>Border-radius:</span>
+            <span
+              className={styles.handle}
+              data-handle="left-bottom"
+              style={{ bottom: `${leftBottom}%` }}
+            />
 
-          <span className={styles.copyText}>
-            10% 10% 10% 10% / 10% 10% 10% 10%
-          </span>
+            <span
+              className={styles.handle}
+              data-handle="left-top"
+              style={{ top: `${leftTop}%` }}
+            />
+          </div>
 
-          <button className={styles.copyButton} type="button">Copy</button>
-        </div>
-      </main>
+          <div className={styles.copyBar}>
+            <span className={styles.copyLabel}>Border-radius:</span>
+
+            <span className={styles.copyText}>
+              {borderRadius}
+            </span>
+
+            <button className={styles.copyButton} type="button">Copy</button>
+          </div>
+        </main>
+      </div>
     </div>
-  </div>
-);
+  );
+};
